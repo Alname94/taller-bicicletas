6d543ff44933630d5d1aee5c5c52f03543772801
Alname94 <alname94@gmail.com>
2026-01-04 16:31:18 -0300

    Primer commit

---
+++ taller-bicicletas/src/main/java/com/alejo/mendez/taller_bicicletas/TallerBicicletasApplication.java
@@ -0,0 +1,13 @@
+package com.alejo.mendez.taller_bicicletas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerBicicletasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerBicicletasApplication.class, args);
+	}
+
+}
---
+++ taller-bicicletas/src/test/java/com/alejo/mendez/taller_bicicletas/TallerBicicletasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejo.mendez.taller_bicicletas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerBicicletasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
